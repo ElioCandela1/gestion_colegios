d2e4aa22c78b09448837a8a5d371221a44bde27a
ElioCandela <eliorubencandela@hotmail.com>
2026-05-29 07:13:16 -0500

    Inicio

---
+++ src/main/java/com/sistema_colegios/gestion_colegios/GestionColegiosApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_colegios.gestion_colegios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionColegiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionColegiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema_colegios/gestion_colegios/GestionColegiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_colegios.gestion_colegios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionColegiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
