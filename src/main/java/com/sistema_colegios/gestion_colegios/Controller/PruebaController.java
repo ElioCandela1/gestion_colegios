9d266a47ddbb51934576ad6efa2436154e51fcca
ElioCandela <eliorubencandela@hotmail.com>
2026-06-03 00:17:52 -0500

    Se configuró el endpoint para cerrar sesion

--- src/main/java/com/sistema_colegios/gestion_colegios/Controller/PruebaController.java
+++ src/main/java/com/sistema_colegios/gestion_colegios/Controller/PruebaController.java
@@ -3,11 +3,16 @@ package com.sistema_colegios.gestion_colegios.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class PruebaController {
 
     @GetMapping("/prueba")
-    public String mostrarPrueba() {
+    public String mostrarPrueba(HttpSession session) {
+        if (session.getAttribute("usuarioLogeado") == null) {
+            return "redirect:/";
+        }
         return "prueba";
     }
 }
